ba03bdb1e3bd5a116646d821585db97320ff80e8
modex9 <modestas.slivinskas@gmail.com>
2020-09-13 21:05:06 +0300

    added module tabs

--- somemodule.php
+++ somemodule.php
@@ -14,7 +14,67 @@ class somemodule extends Module {
     }
 
     public function install() {
-        return parent::install() && $this->registerHook('backofficeHeader');
+        return parent::install() && $this->registerHook('backofficeHeader') && $this->installAdminTabs();
+    }
+
+    public function uninstall() {
+        return parent::uninstall() && $this->uninstallAdminTabs();
+    }
+
+    protected function getAdminTabs()
+    {
+        $tabs = [
+            ['name'=>$this->l('Custom Module Manager'), 'class_name' => 'AdminModuleManagerMain', 'id_parent' => 'AdminParentModulesSf', 'active' => true],
+            ['name'=>$this->l('Module List'), 'class_name' => 'AdminModuleList', 'id_parent' => 'AdminModuleManagerMain', 'active' => true],
+            ['name'=>$this->l('Manager Settings'), 'class_name' => 'AdminModuleManagerSettings', 'id_parent' => 'AdminModuleManagerMain', 'active' => true],
+        ];
+        return $tabs;
+    }
+
+    protected function installAdminTabs()
+    {
+        $available_lang = Language::getLanguages();
+
+        foreach ($this->getAdminTabs() as $tab)
+        {
+            $admin_tab = new Tab();
+            $admin_tab->module = $this->name;
+            $admin_tab->class_name = $tab['class_name'];
+            $admin_tab->id_parent = Tab::getIdFromClassName($tab['id_parent']);
+            $admin_tab->active = $tab['active'];
+
+            foreach ($available_lang as $lang)
+            {
+                $admin_tab->name[$lang['id_lang']] = $tab['name'];
+            }
+
+            if (!$admin_tab->save())
+            {
+                $this->_errors[] = $this->l('Unable to install admin tab: '. $tab['class_name']);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected function uninstallAdminTabs()
+    {
+        foreach ($this->getAdminTabs() as $tab)
+        {
+            $id_tab = Tab::getIdFromClassName($tab['class_name']);
+            if ($id_tab)
+            {
+                $admin_tab = new Tab($id_tab);
+
+                if (!Validate::isLoadedObject($admin_tab)) {
+                    if (!$admin_tab->delete()) {
+                        $this->_errors[] = $this->l('Unable to delete admin tab: ' . $tab['class_name']);
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public function getContent() {
